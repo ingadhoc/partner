8027f48c9ef8e5aec3239b2b9b63afef11fdd73d
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-07-23 16:38:00 -0300

    REF partner module

--- partner_person/__openerp__.py
+++ partner_person/__openerp__.py
@@ -24,28 +24,7 @@
     'category': 'Tools',
     'sequence': 14,
     'summary': '',
-    'description': """
-Partners Persons Management
-===========================
-
-Openerp consider a person those partners that have not "is_company" as true, now, those partners can have:
-----------------------------------------------------------------------------------------------------------
-
-* First Name and Last Name 
-* Birthdate
-* Sex
-* Mother and Father
-* Childs
-* Age (functional field)
-* Nationality
-* Husband/Wife
-* National Identity
-* Passport
-* Marital Status
-
-It also adds a configuration menu for choosing which fields do you wanna see.
-    """,
-    'author':  'ADHOC SA',
+    'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
     'images': [
