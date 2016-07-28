af53d451e30d856d5527cf58dd0149dc170f34b2
susport <richard.pompa84@gmail.com>
2016-07-28 20:17:25 -0300

    9.0-mig-partner_no_auto_search
    Partner No Auto Search

--- partner_no_auto_search/__openerp__.py
+++ partner_no_auto_search/__openerp__.py
@@ -24,10 +24,6 @@
     'category': 'Tools',
     'sequence': 14,
     'summary': '',
-    'description': """
-Partner No Auto Search
-======================
-    """,
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
@@ -43,7 +39,7 @@ Partner No Auto Search
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
