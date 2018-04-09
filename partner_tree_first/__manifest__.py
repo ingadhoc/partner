26143dc679ef1b8d074a848e08ba4baf394055c5
Valentino <vd@adhoc.com.ar>
2018-04-09 08:05:27 -0300

    [MIG] Migration code to v11. [ADD] New folder 'views' for res_partner_view.xml [FIX] Update Readme

--- partner_tree_first/__manifest__.py
+++ partner_tree_first/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Partner Tree First',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
     'sequence': 14,
     'summary': '',
@@ -32,13 +32,13 @@
         'base',
     ],
     'data': [
-        'action_data.xml',
+        'views/res_partner_view',
     ],
     'demo': [
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
