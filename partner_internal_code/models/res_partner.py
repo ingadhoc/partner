0b46466befd17262f7ab81812d4e6c7f8f3897df
Valentino <vd@adhoc.com.ar>
2018-04-10 08:44:55 -0300

    [FIX] Blank lines

--- partner_internal_code/models/res_partner.py
+++ partner_internal_code/models/res_partner.py
@@ -4,8 +4,8 @@
 ##############################################################################
 from odoo import fields, models, api
 
+
 class Partner(models.Model):
-    
 
     _inherit = 'res.partner'
 
