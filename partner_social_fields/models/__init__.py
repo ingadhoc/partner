d82daf957cb15803fa700c37f76986c5b60f13b4
ValentinoDefelice <vd@adhoc.com.ar>
2018-04-10 13:50:02 -0300

    [REM] partner_social_fields deprecated for version 11.0 (#34)

--- partner_social_fields/models/__init__.py
+++
@@ -1,5 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import partner_social_fields
