c2eb9b37b2c1191f26f58923c1b3be0a95e807b6
ValentinoDefelice <vd@adhoc.com.ar>
2018-04-26 09:00:38 -0300

    [MIG] Migration Internal Code to v11. (#33)
    * [MIG] Migration code to v11.
    [ADD] Views, Data & Models folder. Add files in respective folders.
    [FIX] Update Readme.
    
    * [REM] Remove conflict id in res.partner.csv
    
    * [FIX] Readme module description
    [FIX] Replace data tag for odoo
    [REM] Blank lines and shebang
    [FIX] Replace new base demo user record
    
    * [FIX] Blank lines

--- partner_internal_code/models/res_partner.py
+++ partner_internal_code/models/res_partner.py
@@ -5,9 +5,7 @@
 from odoo import fields, models, api
 
 
-class partner(models.Model):
-
-    """"""
+class Partner(models.Model):
 
     _inherit = 'res.partner'
 
@@ -36,7 +34,7 @@ class partner(models.Model):
         if not vals.get('internal_code', False):
             vals['internal_code'] = self.env[
                 'ir.sequence'].next_by_code('partner.internal.code') or '/'
-        return super(partner, self).create(vals)
+        return super(Partner, self).create(vals)
 
     _sql_constraints = {
         ('internal_code_uniq', 'unique(internal_code)',
