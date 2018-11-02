e5c65b27d80f8e3359744f23b9ebfe92db4302e9
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-11-02 13:32:38 -0300

    Revert "[REM] partner_sales_abc deprecated for version 11.0 (#35)"
    This reverts commit d308cd7263f7f72ab068fed6c9974a5edc25dd74.

--- partner_sales_abc/__manifest__.py
+++ partner_sales_abc/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Partner Sales ABC',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'website': 'www.adhoc.com.ar',
     'author': 'ADHOC SA',
     'license': 'AGPL-3',
@@ -32,7 +32,7 @@
         'views/partner_view.xml',
     ],
     'test': [],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': True,
 }
