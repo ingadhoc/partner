2d7dbb67fc9a1eca87ad25fd75672dd4d4c94fc9
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-04-08 10:33:24 -0300

    [PORT] from 9.0 partner_state module (#50)
    * [FIX] add aprove partner group to "To potencial" button (#49)

--- partner_state/__manifest__.py
+++ partner_state/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Partner State',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'category': 'Base',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
