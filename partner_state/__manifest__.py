e09f83449a63e11985becca7fcaec3890ae0373b
docker-odoo <lef@adhoc.com.ar>
2023-12-27 12:56:21 +0000

    [MIG] partner_state: Migration to 17.0

--- partner_state/__manifest__.py
+++ partner_state/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Partner State',
-    'version': "16.0.1.0.0",
+    'version': "17.0.1.0.0",
     'category': 'Base',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
@@ -40,5 +40,5 @@
     'demo': [
         'demo/res.partner.state_field.csv'
     ],
-    'installable': False,
+    'installable': True,
 }
