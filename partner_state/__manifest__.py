ca49ca2dc8184f2fc924cd79a116ab114e03dc29
mav-adhoc <mav@adhoc.com.ar>
2024-03-13 13:10:22 -0300

    [ADD] partner_state: chatter box field tracking

--- partner_state/__manifest__.py
+++ partner_state/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Partner State',
-    'version': "16.0.1.0.0",
+    'version': "16.0.1.1.0",
     'category': 'Base',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
