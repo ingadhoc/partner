0f54f7006a9e454d09c9351d153b737af8f29566
docker-odoo <docker-odoo@example.com>
2024-10-18 19:01:11 +0000

    [MIG] partner_tree_first: Migration to 18.0

--- partner_tree_first/models/ir_actions_act_window.py
+++ partner_tree_first/models/ir_actions_act_window.py
@@ -7,17 +7,17 @@ class IrActionsActWindow(models.Model):
 
     @api.depends('view_ids.view_mode', 'view_mode', 'view_id.type')
     def _compute_views(self):
-        """ Force the tree view to always be the first one, no matter what is
+        """ Force the list view to always be the first one, no matter what is
         configured in the action """
         super()._compute_views()
-        res_partner = self.sudo().filtered(lambda x: x.res_model == 'res.partner' and 'tree' in x.view_mode)
+        res_partner = self.sudo().filtered(lambda x: x.res_model == 'res.partner' and 'list' in x.view_mode)
         for act in res_partner.with_context(from_config=True):
             modes = act.view_mode.split(',')
-            modes.remove('tree')
-            modes = ['tree'] + modes
+            modes.remove('list')
+            modes = ['list'] + modes
             act.view_mode = ','.join(modes)
             for item in act.views:
-                if item[1] == 'tree':
+                if item[1] == 'list':
                     list_view = item
                     act.views.remove(item)
                     act.views = [list_view] + act.views
