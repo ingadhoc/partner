b1bbec4795bd6cdc0b782fb0a8e1f9c3cb60b269
mav-adhoc <mav@adhoc.com.ar>
2025-05-15 14:54:12 +0000

    [ADD]partner_creation_access: new module in 18.0
    closes ingadhoc/partner#128
    
    Signed-off-by: ced-adhoc <ced@adhoc.com.ar>

---
+++ partner_creation_access/models/res_partner.py
@@ -0,0 +1,14 @@
+from odoo import api, models
+from odoo.exceptions import UserError
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    @api.model_create_multi
+    def create(self, vals):
+        if not self.env.user.has_group("base.group_partner_manager"):
+            raise UserError(
+                "You don't have access to create contacts. Only users with the 'Contact Creation' access can do it."
+            )
+        return super().create(vals)
